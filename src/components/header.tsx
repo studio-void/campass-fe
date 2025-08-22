75335a018826614c6a9966751158df190ca35147
Cyan <hoony6134@gmail.com>
2025-08-23 02:55:30 +0900

    feat: design header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,8 @@ import { forwardRef } from 'react';
 
 import { Link } from '@tanstack/react-router';
 
+import { Button } from './ui/button';
+
 export const Header = forwardRef<
   HTMLElementTagNameMap['header'],
   React.HTMLAttributes<HTMLElementTagNameMap['header']>
@@ -20,7 +22,56 @@ export const Header = forwardRef<
               className="h-8"
             />
           </Link>
-          <div className="flex-grow" />
+        </div>
+        <div className="flex-grow" />
+        <div className="flex items-center gap-12 text-sm">
+          <Link
+            to="/"
+            className="hover:font-semibold transition-all relative after:content-[''] after:block after:h-[2px] after:bg-current after:w-0 hover:after:w-full after:transition-all after:duration-300 after:absolute after:left-0 after:-bottom-1 after:origin-left dark:text-neutral-50"
+          >
+            About
+          </Link>
+          <Link
+            to="/"
+            className="hover:font-semibold transition-all relative after:content-[''] after:block after:h-[2px] after:bg-current after:w-0 hover:after:w-full after:transition-all after:duration-300 after:absolute after:left-0 after:-bottom-1 after:origin-left dark:text-neutral-50"
+          >
+            Dormitory
+          </Link>
+          <Link
+            to="/"
+            className="hover:font-semibold transition-all relative after:content-[''] after:block after:h-[2px] after:bg-current after:w-0 hover:after:w-full after:transition-all after:duration-300 after:absolute after:left-0 after:-bottom-1 after:origin-left dark:text-neutral-50"
+          >
+            Research
+          </Link>
+          <Link
+            to="/"
+            className="hover:font-semibold transition-all relative after:content-[''] after:block after:h-[2px] after:bg-current after:w-0 hover:after:w-full after:transition-all after:duration-300 after:absolute after:left-0 after:-bottom-1 after:origin-left dark:text-neutral-50"
+          >
+            Team Project
+          </Link>
+          <Link
+            to="/"
+            className="hover:font-semibold transition-all relative after:content-[''] after:block after:h-[2px] after:bg-current after:w-0 hover:after:w-full after:transition-all after:duration-300 after:absolute after:left-0 after:-bottom-1 after:origin-left dark:text-neutral-50"
+          >
+            Facility
+          </Link>
+          <Link
+            to="/"
+            className="hover:font-semibold transition-all relative after:content-[''] after:block after:h-[2px] after:bg-current after:w-0 hover:after:w-full after:transition-all after:duration-300 after:absolute after:left-0 after:-bottom-1 after:origin-left dark:text-neutral-50"
+          >
+            Timetable
+          </Link>
+          <Link
+            to="/"
+            className="hover:font-semibold transition-all relative after:content-[''] after:block after:h-[2px] after:bg-current after:w-0 hover:after:w-full after:transition-all after:duration-300 after:absolute after:left-0 after:-bottom-1 after:origin-left dark:text-neutral-50"
+          >
+            Wiki
+          </Link>
+          <Link to="/">
+            <Button variant="default" className="font-semibold">
+              Get Started
+            </Button>
+          </Link>
         </div>
       </nav>
     </header>
