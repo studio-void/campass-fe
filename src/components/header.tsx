89dccbda07d24051cf8d57e146f69abbb11ceb0b
Cyan <hoony6134@gmail.com>
2025-08-23 02:04:37 +0900

    feat: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,16 +7,18 @@ export const Header = forwardRef<
   React.HTMLAttributes<HTMLElementTagNameMap['header']>
 >((_, ref) => {
   return (
-    <header
-      ref={ref}
-      className="p-2 flex gap-2 bg-white text-black justify-between"
-    >
-      <nav className="flex flex-row">
+    <header className="bg-white/20 dark:bg-black/20 backdrop-blur-lg border-b border-white/30 dark:border-black/30 fixed top-0 left-0 w-full z-50">
+      <nav className="mx-6 md:mx-12 lg:mx-24 xl:mx-48 py-4 flex flex-row items-center">
         <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img
+              src="/images/campass_wordmark.svg"
+              alt="Campass Logo"
+              className="h-8"
+            />
+          </Link>
+          <div className="flex-grow" />
         </div>
-
-        <div className="px-2 font-bold">TanStack Query</div>
       </nav>
     </header>
   );
