df642ae44ef2a96df14700fd2de162a2f3c6cc1e
Hyunseung Ryu <rhseungg@gmail.com>
2025-08-24 05:44:34 +0900

    feat: home page redesign

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,7 @@ export const Header = forwardRef<
   return (
     <header
       ref={ref}
-      className="bg-white/20 dark:bg-black/20 backdrop-blur-lg border-b border-white/30 dark:border-black/30 fixed top-0 left-0 w-full z-50"
+      className="backdrop-blur-lg fixed top-0 left-0 w-full z-50"
     >
       <nav className="mx-6 md:mx-12 lg:mx-24 xl:mx-48 py-4 flex flex-row items-center">
         <div className="font-bold">
